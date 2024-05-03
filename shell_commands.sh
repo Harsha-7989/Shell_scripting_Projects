a519fd712759c3f589194c4114277388623e0b6a
Harsha-7989 <mukkamallaharshavardhan@gmail.com>
2024-05-04 00:15:02 +0530

    added wegt, curl, find,ifelse,forloop

--- shell_commands.sh
+++ shell_commands.sh
@@ -1,4 +1,7 @@
- Comments to write the imfornation
+#!/bin/bash
+
+
+Comments to write the imfornation
 
  set -x # debug mode
 
@@ -21,6 +24,54 @@ date | echo "today is " # it will the output are expecting because Date is a sys
 
 
 ps -ef | grep "harsha" | awk -F" " '{print $4}' # awk command can be used for pattern searching
+ 
+
+#logfile  - to find errors in logfile
+
+# use 'curl links_of_logfile | gerp error' to get get imformation from external sources
+
+# wget is more like a download command
 
 
 
+
+#if[expression]
+#then
+#	sdfgjfdjklds
+#	fdsfd
+#else
+#	fhjrfjkajslk
+
+#fi
+
+
+
+a=4
+b=10
+
+if[ $a > $b ]
+then
+	echo "a is greater than b"
+else
+	echo "b is greater than a"
+fi
+
+
+# iterations-->we use for loop;
+
+# while, for, until, select
+
+
+for i in {1.100}; do echo $i; done
+
+
+
+# for
+# do 
+#	increment
+# done
+
+
+
+
+ 
